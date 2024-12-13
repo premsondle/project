bab5628e9145c9f6e42e4cff92d1b93682542521
Your Name <om.pawar1512@gamil.com>
2024-12-14 00:11:40 +0530

    menu box

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,6 +1,41 @@
-let loadingfunc = () =>{
+let loadingfunc = () => {
+  setTimeout(() => {
+    document.getElementById("top-content3").classList.remove("loading");
+  }, 1000);
+};
 
-    setTimeout(() => {
-        document.getElementById("top-content3").classList.remove("loading");
-    },1000);    
+function openMenu() {
+  document.getElementById("menu-container").style.zIndex = 20;
+  document.getElementById("menu-wrapper").style.zIndex = 10;
+  document.getElementById("menu-wrapper").classList.remove("not-active");
+  document.getElementById("menu-container").classList.remove("not-active");
+  setTimeout(() => {
+    document.getElementById("inner-content").classList.remove("not-active");
+  }, 300);
+  setTimeout(() => {
+    document.getElementById("menu-links").style.opacity = 1;
+    document.getElementById("menu-links").style.transform = "translateY(0)";
+  }, 400);
+}
+
+function closeMenu() {
+
+
+  document.getElementById("menu-links").style.opacity = 0;
+  document.getElementById("menu-links").style.transform = "translateY(4rem)";
+
+  setTimeout(() => {
+    document.getElementById("menu-wrapper").classList.add("not-active");
+    document.getElementById("menu-container").classList.add("not-active");
+  }, 500);
+
+  setTimeout(() => {
+    document.getElementById("menu-container").style.zIndex = 0;
+    document.getElementById("inner-content").classList.remove("not-active");
+
+  }, 600);
+  setTimeout(() => {
+    document.getElementById("menu-wrapper").style.zIndex = 0;
+    document.getElementById("inner-content").classList.add("not-active");
+  }, 700);
 }
